a22e092e570c9b93e42bef2c9d9c3a34a1e8a9f6
SandeshDhakal0 <dhakalsandesh0@gmail.com>
2022-06-19 13:31:27 +0545

    update page

--- auth/edit.php
+++ auth/edit.php
@@ -27,6 +27,7 @@
                             //notify
                             header("location: list.php");
                             exit;
+                        }
                         } else {
                             //
                             header("location: list.php");
@@ -40,21 +41,14 @@
                 <div class="form-group row mb-3">
                         <label for="" class="col-sm-12 col-md-3">Name: </label>
                         <div class="col-sm-12 col-md-9">
-                            <input type="text" class="form-control form-control-sm" name="name" required placeholder="Enter your full name...">
+                            <input type="text" value="<?php echo $user_info['name']?>" class="form-control form-control-sm" name="name" required placeholder="Enter your full name...">
                         </div>
                     </div>
 
                     <div class="form-group row mb-3">
                         <label for="" class="col-sm-12 col-md-3">Email: </label>
                         <div class="col-sm-12 col-md-9">
-                            <input type="email" class="form-control form-control-sm" name="email" required placeholder="Enter your email...">
-                        </div>
-                    </div>
-
-                    <div class="form-group row mb-3">
-                        <label for="" class="col-sm-12 col-md-3">Password: </label>
-                        <div class="col-sm-12 col-md-9">
-                            <input type="password" class="form-control form-control-sm" name="name" required placeholder="Enter your password...">
+                            <input type="email" value="<?php echo $user_info['email']?>" class="form-control form-control-sm" name="email" required placeholder="Enter your email...">
                         </div>
                     </div>
 
@@ -62,15 +56,15 @@
                         <label for="" class="col-sm-12 col-md-3">Gender: </label>
                         <div class="col-sm-12 col-md-9">
                             <label for="male">
-                            <input type="radio" name="gender" value="Male" id="male">Male
+                            <input type="radio" name="gender" value="Male" id="male" <?php echo ($user_info['gender'] == 'Male') ? 'checked' : '' ?>>Male
                             </label>
 
                             <label for="female">
-                            <input type="radio" name="gender" value="Female" id="female">Female
+                            <input type="radio" name="gender" value="Female" id="female" <?php echo ($user_info['gender'] == 'Female') ? 'checked' : ''?>>Female
                             </label>
 
                             <label for="other">
-                            <input type="radio" name="gender" value="Other" id="other">Other
+                            <input type="radio" name="gender" value="Other" id="other" <?php echo ($user_info['gender'] == 'Other') ? 'checked' : ''?>>Other
                             </label>
                         </div>
                     </div>
@@ -80,9 +74,9 @@
                         <div class="col-sm-12 col-md-9">
                             <select name="address" id="address" class="form-control form-control-sm">
                                 <option value="" selected disabled>---Select Any One---</option>
-                                <option value="Kathmandu">Kathmandu</option>
-                                <option value="Bhaktapur">Bhaktapur</option>
-                                <option value="Lalitpur">Lalitpur</option>
+                                <option value="Kathmandu" <?php echo ($user_info['address'] == 'Kathmandu') ? 'selected' : '' ?>>Kathmandu</option>
+                                <option value="Bhaktapur" <?php echo ($user_info['address'] == 'Bhaktapur') ? 'selected' : '' ?>>Bhaktapur</option>
+                                <option value="Lalitpur" <?php echo ($user_info['address'] == 'Lalitpur') ? 'selected' : '' ?>>Lalitpur</option>
                             </select>
                         </div>
                     </div>
@@ -90,21 +84,30 @@
                     <div class="form-group row mb-3">
                         <label for="" class="col-sm-12 col-md-3">Date of Birth: </label> 
                         <div class="col-sm-12 col-md-9">
-                            <input type="date" class="form-control form-control-sm" name="dob">
+                            <input type="date" class="form-control form-control-sm" name="dob" value="<?php echo $user_info['date_of_birth']?>">
                     </div> 
                 </div>
 
                     <div class="form-group row mb-3">
                         <label for="" class="col-sm-12 col-md-3">Image: </label> 
-                        <div class="col-sm-12 col-md-9">
+                        <div class="col-sm-12 col-md-3">
                         <input type="file" name="image" accept="image/*"> 
                     </div> 
+                    <div class="col-sm-12 col-md-3">
+                        <?php
+                            if($user_info['image'] != null){
+                                ?>
+                                <img src="uploads/image/<?php echo $user_info['image']?>" alt="" class="img-fluid img-thubnail">
+                                <?php
+                            }
+                        ?>
+
+                    </div>
                 </div>
                     <div class="form-group row mla-3">
                         <label for="" class="col-sm-12 col-md-3">About: </label>
                         <div class="col-sm-12 col-md-9">
-                        <textarea name="about" id="about" class="form-control form-control-sm" rows="5"
-                        style="resize: none;"></textarea> 
+                        <textarea name="about" id="about" class="form-control form-control-sm" rows="5" style="resize:none;"><?php echo $user_info['about']?></textarea> 
                     </div>
                  </div>
 
