b292c8c64308aa41674299894132a6929d8e8ee9
SandeshDhakal0 <dhakalsandesh0@gmail.com>
2022-06-21 09:08:27 +0545

    logout using session destroy

--- auth/dashboard.php
+++ auth/dashboard.php
@@ -3,9 +3,11 @@ session_start();
 if(isset($_SESSION['user_id']) || empty($_SESSION['user_id'])){
     //
     $_SESSION['error'] = "Please login first.";
-    header('location: ./');
+    header('location: ./login.php');
     exit;
 } else {
     echo "Welcome to admin profile,".$_SESSION['name'];
 }
 ?>
+
+<a href="logout.php">Logout</a>
