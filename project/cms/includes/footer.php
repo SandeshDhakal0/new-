79f2aa7106d1f8cb37a3a73b62889a5affb05621
SandeshDhakal0 <dhakalsandesh0@gmail.com>
2022-06-24 11:27:47 +0545

    Clean template

--- project/cms/includes/footer.php
+++ project/cms/includes/footer.php
@@ -19,14 +19,14 @@
     </div>
  
  <!-- Bootstrap core JavaScript-->
- <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+ <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
 
     <!-- Core plugin JavaScript-->
-    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="assets/js/jquery.easing.min.js"></script>
 
     <!-- Custom scripts for all pages-->
-    <script src="js/sb-admin-2.min.js"></script>
+    <script src="assets/js/sb-admin-2.min.js"></script>
 
 </body>
 
