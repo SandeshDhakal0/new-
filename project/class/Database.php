5c81d95263ae7f80c3bb498d3805a924b2dafa99
SandeshDhakal0 <dhakalsandesh0@gmail.com>
2022-06-26 13:43:55 +0545

    Exception handling

--- project/class/Database.php
+++ project/class/Database.php
@@ -4,13 +4,14 @@
         
         function __construct(){
         try{
-            $this->conn = new PDO("mysql:host=localhost;dbname=php_brodway,'root',''");
+            $this->conn = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER,DB_PWD);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch(PDOException $ex){
             $msg = date("Y-m-d H:i:s").",Connection: ".$ex->getMessage()."\n\r";
-            error_log($msg, 3, '../error/error.log');
+            error_log($msg, 3, ERROR_LOG);
         } catch(Exception $ex){
             $msg = date("Y-m-d H:i:s") . ", Connection:" .$ex-getMessage()."\n\r";
+            error_log($msg, 3, ERROR_LOG);
         }
     } 
 
