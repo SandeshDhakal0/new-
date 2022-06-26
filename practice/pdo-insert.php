4a08e52ab4274b12e9fd29908edff62cc4a24b96
SandeshDhakal0 <dhakalsandesh0@gmail.com>
2022-06-26 19:40:25 +0545

    Data binding, Sql Injection

--- practice/pdo-insert.php
+++ practice/pdo-insert.php
@@ -2,6 +2,7 @@
 
     $conn = new PDO("mysql:host=localhost;dbname=php_brodway;","root","");
     //error==> exception
+    //insert
     $year = "078";
     $name = "Hehehe";
     $email = "heheh@gmail.com";
@@ -12,18 +13,30 @@
     $about = "Here I am.";
     $password = password_hash("admin123", PASSWORD_BCRYPT);
 
-    // insert query
+    // sql injection
+    // $sql = "INSERT INTO users SET
+    //         year = '$year',
+    //         name = '$name',
+    //         email = '$email',
+    //         password = '$password',
+    //         role_id = '$role_id',
+    //         gender = '$gender',
+    //         address = '$address',
+    //         date_of_birth = '$dob',
+    //         about = '$about'
+    //         ";
+
     $sql = "INSERT INTO users SET
-            year = '$year',
-            name = '$name',
-            email = '$email',
-            password = '$password',
-            role_id = '$role_id',
-            gender = '$gender',
-            address = '$address',
-            date_of_birth = '$dob',
-            about = '$about'
-            ";
+        year = :year,
+        name = :name,
+        email = :email,
+        password = :password,
+        role_id = :role_id,
+        gender = :gender,
+        address = :address,
+        date_of_birth = :date_of_birth,
+        about = :about
+        ";
 
     //Convert sql to statement object
     $stmt = $conn->prepare($sql);
@@ -33,4 +46,20 @@
         echo "Error in statement conversion";
         exit;
     }
-    
+//This is data binding
+    $stmt->bindValue(":year", $year, PDO::PARAM_STR);
+    $stmt->bindValue(":name", $name, PDO::PARAM_STR);
+    $stmt->bindValue(":email", $email, PDO::PARAM_STR);
+    $stmt->bindValue(":password", $password, PDO::PARAM_STR);
+    $stmt->bindValue(":role_id", $role_id, PDO::PARAM_INT);
+    $stmt->bindValue(":gender", $gender, PDO::PARAM_STR);
+    $stmt->bindValue(":address", $address, PDO::PARAM_STR);
+    $stmt->bindValue(":date_of_birth", $dob, PDO::PARAM_STR);
+    $stmt->bindValue(":about", $about, PDO::PARAM_STR);
+    //execute this statement
+    $success = $stmt->execute();
+    if($success){
+        echo "Data entered successfully.";
+    } else {
+        echo "There was a problem while entering the data.";
+    }
