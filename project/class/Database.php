d947f2d7fd10cb74769f46fa1bc9799f19b00427
SandeshDhakal0 <dhakalsandesh0@gmail.com>
2022-06-29 16:35:15 +0545

    commit

--- project/class/Database.php
+++ project/class/Database.php
@@ -25,8 +25,24 @@
             error_log($msg, 3, ERROR_LOG);
         }
     } 
-    final protected function select($data){
+    final protected function select($data = array()){
         try{
+            $this->stmt = $this->conn->prepare($this->sql);
+            if(!empty($data)){
+                foreach($data as $key => $value){
+                    if(is_int($value)){
+                        $param_type = PDO::PARAM_INT;
+                    } else if(is_bool($value)){
+                        $param_type = PDO::PARAM_BOOL;
+                    }else {
+                        $param_type = PDO::PARAM_STR;
+                    }
+                    $this->stmt->bindValue(":".$key, $value, $param_type);
+                }
+            }
+            $this->stmt->execute();
+            $result = $this->stmt->fetchAll(PDO::FETCH_OBJ);
+            return $result;
 
         } catch(PDOException $ex){
             $msg = date("Y-m-d H:i:s").",Select: ".$ex->getMessage()."\n\r";
